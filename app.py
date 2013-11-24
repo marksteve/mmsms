4ce66cd9d1dd5f98c2d5b6073141ac1abd0d01c1
Mark Steve Samson <hello@marksteve.com>
2013-11-24 09:05:04 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,97 @@
+from flask import (
+  Flask,
+  request,
+)
+from simpleflake import simpleflake
+import json
+import random
+import redis
+import requests
+
+
+SHORTCODE = "21589973"
+
+
+fl = Flask(__name__)
+db = redis.StrictRedis()
+
+
+def key(*a):
+  return ":".join(map(str, a))
+
+
+def get_user_token(user_id):
+  return db.get(key("users", user_id))
+
+
+def send(user, message):
+  sender = SHORTCODE[-4:]
+  access_token = get_user_token(user)
+  params = dict(access_token=access_token)
+  payload = json.dumps(dict(
+    outboundSMSMessageRequest=dict(
+    clientCorrelator=simpleflake(),
+    senderAddress="tel:%s" % sender,
+    outboundSMSTextMessage=dict(message=message),
+    address=["tel:+63%s" % user],
+  )))
+  # TODO: Check response
+  requests.post(
+    "http://devapi.globelabs.com.ph/smsmessaging/v1/outbound/%s/requests" % sender,
+    headers={"Content-Type": "application/json"},
+    params=params,
+    data=payload,
+  )
+
+
+@fl.route("/subscribe", methods=["GET"])
+def subscribe():
+  u = request.args["subscriber_number"]
+  k = key("users", u)
+  # Set user access token
+  db.set(k, request.args["access_token"])
+  return ""
+
+
+@fl.route("/receive", methods=["POST"])
+def receive():
+  for m in request.json["inboundSMSMessageList"]["inboundSMSMessage"]:
+    s = m["message"].strip().upper().split(" ")
+    num = m["senderAddress"].replace("tel:+63", "")
+
+    if s[0] == "CREATE":
+      id = simpleflake()
+      db.set(key("mmowners", id), num)
+
+    if s[0] == "JOIN":
+      id = s[1]
+      name = s[2]
+      db.sadd(key("mmgroups", id), key(num, name))
+
+    if s[0] == "DRAW":
+      g = [db.smembers(key("mmgroups", id))]
+      ordered = []
+      while g:
+        m = random.choice(g)
+        g.remove(m)
+        ordered.append(m)
+      ol = len(ordered)
+      ordered.append(ordered[0])
+      for i, m in enumerate(ordered):
+        num, name = m.split(":", 1)
+        pnum, pname = ordered[i + 1].split(":", 1)
+        send(num, "You're MONITO MONITA is %s!", pname)
+        if i >= ol - 1:
+          break
+
+    break
+  return ""
+
+
+def main():
+  fl.run(host="0.0.0.0", debug=True)
+
+
+if __name__ == '__main__':
+  main()
+
