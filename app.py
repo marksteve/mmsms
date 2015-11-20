9a7e39ec18b78de434305bc1936772044b56826f
Mark Steve Samson <hello@marksteve.com>
2015-11-20 12:30:49 +0800

    Add rate limiting

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import base64
+import math
 import os
 import random
+import time
+from functools import wraps
 
 import requests
 from flask import abort, Flask, render_template, request, session
 from redis import StrictRedis
 from simpleflake import simpleflake
+from werkzeug.exceptions import TooManyRequests
 
 CHIKKA_API_URL = "https://post.chikka.com/smsapi/request"
 
@@ -19,25 +23,41 @@ db = StrictRedis(host=os.environ.get("REDIS_HOST", "localhost"))
 
 
 def send_sms(mobile_number, message):
-  requests.post(
-    CHIKKA_API_URL,
-    data=dict(
-      message_type="SEND",
-      mobile_number=mobile_number,
-      shortcode=SHORTCODE,
-      message_id=str(simpleflake()),
-      message=message,
-      client_id=CLIENT_ID,
-      secret_key=SECRET_KEY,
+    requests.post(
+        CHIKKA_API_URL,
+        data=dict(
+            message_type="SEND",
+            mobile_number=mobile_number,
+            shortcode=SHORTCODE,
+            message_id=str(simpleflake()),
+            message=message,
+            client_id=CLIENT_ID,
+            secret_key=SECRET_KEY,
+        )
     )
-  )
 
 
 def gen_csrf_token():
-  return base64.b64encode(os.urandom(32))
+    return base64.b64encode(os.urandom(32))
+
+
+def rate_limit(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        last_req = db.get("last_req:" + request.remote_addr)
+        if request.method == "POST" and last_req:
+            limit_end = int(last_req) + 30 * 60
+            remaining = limit_end - int(time.time())
+            if remaining > 0:
+                raise TooManyRequests("Try again in {} minutes.".format(
+                    int(math.ceil(remaining / 60.))
+                ))
+        return f(*args, **kwargs)
+    return decorated
 
 
 @app.route("/", methods=["GET", "POST"])
+@rate_limit
 def index():
     context = {}
     if request.method == "GET":
@@ -52,9 +72,12 @@ def index():
             abort(401)
         names = request.form.getlist("name")
         numbers = request.form.getlist("number")
-        theme = request.form.get("theme", "").strip()
-        db.sadd("themes", theme)  # For stats purposes :)
         members = zip(names, numbers)
+        if not members:
+            abort(400)
+        theme = request.form.get("theme", "").strip()
+        # Record theme for stats purposes
+        db.sadd("themes", theme)
         random.shuffle(members)
         for i, [name, number] in enumerate(members):
             assigned_name, _ = members[(i + 1) % len(members)]
@@ -62,7 +85,8 @@ def index():
                 name, assigned_name)
             if theme:
                 message += "Our theme is \"{}\". ".format(theme)
-            send_sms(number, message)
+            # send_sms(number, message)
+        db.set("last_req:" + request.remote_addr, int(time.time()))
         context.update(has_drawn=True, members=members)
         session.clear()
     else:
@@ -74,8 +98,9 @@ def index():
 @app.errorhandler(401)
 @app.errorhandler(404)
 @app.errorhandler(405)
+@app.errorhandler(429)
 def error(error):
-  return render_template("error.html", error=error)
+    return render_template("error.html", error=error)
 
 
 if __name__ == "__main__":
